3f4dac87870090e014555de78e32c869e5c47138
Amit <amitzmn@gmail.com>
2019-01-09 22:04:50 -0500

    Styled main color scheme

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,17 +7,12 @@ export default class Footer extends Component {
         <div className="row">
           <div className="twelve columns">
             <ul className="social-links">
-              <li><a target="_blank" href="#"><i className="fa fa-facebook" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-twitter" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-google-plus" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-linkedin" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-instagram" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-dribbble" /></a></li>
-              <li><a target="_blank" href="#"><i className="fa fa-skype" /></a></li>
+              <li><a target="_blank" href="https://www.linkedin.com/in/amitzaman/"><i className="fa fa-linkedin" /></a></li>
+              <li><a target="_blank" href="https://github.com/amitzed"><i className="fa fa-github" /></a></li>
+              <li><a target="_blank" href="https://amitzaman.com/"><i className="fa fa-globe" /></a></li>
             </ul>
             <ul className="copyright">
               <li>© Copyright 2019 Amit Zaman</li>
-              <li>Design by <a target="_blank" title="AmitZaman" href="https://amitzaman.com/">Amit Zaman</a></li>
             </ul>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
